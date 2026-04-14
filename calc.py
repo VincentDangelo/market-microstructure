c37b3abee1f506bd630b93fda36c3f981602489a
Vincent D'Angelo <Vincentdangelo707@gmail.com>
2026-04-13 23:09:49 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,31 @@
+import math
 
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b / a
+
+
+def log(a, b):
+    # log base a of b
+    if a <= 0 or a == 1:
+        raise ValueError("Log base must be positive and not equal to 1.")
+    if b <= 0:
+        raise ValueError("Log argument must be positive.")
+    return math.log(b, a)
+
+
+def exp(a, b):
+    return a ** b
