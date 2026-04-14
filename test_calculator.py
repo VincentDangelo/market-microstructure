d527d086e64aba972356384cb72ce91f74bd46c2
Vincent D'Angelo <Vincentdangelo707@gmail.com>
2026-04-13 23:18:45 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,25 +1,34 @@
 import math
 
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of a negative number.")
+    return math.sqrt(a)
+
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+
 def add(a, b):
     return a + b
 
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
 
-def div(a, b):
+def divide(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
     return b / a
 
 
-def log(a, b):
-    # log base a of b
+def logarithm(a, b):
     if a <= 0 or a == 1:
         raise ValueError("Log base must be positive and not equal to 1.")
     if b <= 0:
